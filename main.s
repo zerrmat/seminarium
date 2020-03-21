56d417dd31366d3a92af74b2efb928cb23fb1954
zerrmat <zerr@mat.umk.pl>
2020-03-21 14:38:40 +0100

    Appropriate text blinks

--- main.s
+++ main.s
@@ -248,6 +248,44 @@ skip_mainmenu_scroll:
 	lda mainmenuFlags
 	ora #%00000001
 	sta mainmenuFlags
+; drawBlinkText:
+	lda #%00000010
+	and mainmenuFlags
+	bne showBlinkText
+; hideBlinkText:
+	lda $2002
+	lda #$21
+	sta $2006
+	lda #$07
+	sta $2006
+	ldx #$00
+hideBlinkTextLoop:
+	lda #$00
+	sta $2007
+	inx
+	cpx #$11
+	bne hideBlinkTextLoop
+	jmp endBlinkText
+showBlinkText:
+	lda $2002
+	lda #$21
+	sta $2006
+	lda #$07
+	sta $2006
+	ldx #$00
+showBlinkTextLoop:
+	lda mainmenu_pushstart_text, x
+	sta $2007
+	inx
+	cpx #$11
+	bne showBlinkTextLoop
+endBlinkText:
+; fixScroll:
+	lda mainmenuScrollY
+	sta $2005
+	sty $2005
+	lda #%10000000
+	sta $2000
 endNMI:
 	lda #$00
 	sta mainLoopSleeping
@@ -336,6 +374,9 @@ mainmenu_nametable:
 	
 mainmenu_nametable_2:
 	.incbin "Nametables/mainmenu2.nam"
+	
+mainmenu_pushstart_text:
+	.byte $1A, $1F, $1D, $12, $00, $1D, $1E, $0B, $1C, $1E, $00, $0C, $1F, $1E, $1E, $19, $18
 
 .segment "TILES"
     .incbin "CHR/my.chr" ; if you have one
