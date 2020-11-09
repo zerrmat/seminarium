069fffc074684a17ce87e230be3f04678c5be582
zerrmat <zerr@mat.umk.pl>
2020-11-09 16:49:03 +0100

    Further development of BSc thesis

--- PRG/romheader.s
+++ PRG/romheader.s
@@ -4,3 +4,5 @@
 .byte 2			; 2 x 16 KB (32 KB) for code (PRG)
 .byte 1 		; 1 x 8 KB (8 KB) for graphics (CHR)
 .byte %00000000 ; mapper 0 (NROM) (highest 4 bits), vertical mirroring (8th bit)
+.byte 0, 0
+.byte 0, 0, 0, 0, 0, 0
